6d480122623480dbf6a4e10069d2dad290fbd77c
Benjamin Roth <apnea.diving.deep@gmail.com>
2013-08-31 22:45:22 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Toptal::Application.config.secret_key_base = '489f1669236a758263bd82846d72410320dd7b8b284aaabd990db74bce1bb7557c7fcb90ec785468bc820b80fc65770b2247b8bf613d2f43797a6f74eb5a286c'
