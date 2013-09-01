eba0fb1812c447bd4f64b8b33287525931eeb1af
Benjamin Roth <apnea.diving.deep@gmail.com>
2013-09-01 14:21:09 +0200

    add validation and state machine to tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,19 @@
 class Task < ActiveRecord::Base
+
+  STATUSES = %w(pending completed)
+
+  validates :status,      presence: true, inclusion: { in: STATUSES }
+  validates :description, presence: true
+  validates :user_id,     presence: true
+
+  state_machine :status, initial: :pending do
+    event :complete do
+      transition :pending => :completed
+    end
+
+    event :reset do
+      transition :completed => :pending
+    end
+  end
+
 end
